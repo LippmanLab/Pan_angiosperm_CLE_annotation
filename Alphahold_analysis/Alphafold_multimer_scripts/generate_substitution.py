a259951d64070478852f4080631f722e5ce3c41b
Lippman Lab <35844504+LippmanLab@users.noreply.github.com>
2025-09-05 14:10:56 -0400

    Update generate_substitution.py

--- Alphahold_analysis/Alphafold_multimer_scripts/generate_substitution.py
+++ Alphahold_analysis/Alphafold_multimer_scripts/generate_substitution.py
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-# Run the Python script and store the output in a variable
-substitutions=$(python generate_substitutions.py)
-
-# Loop over each substitution and create a directory
-for sub in $substitutions; do
-    mkdir "$sub"
-done
-
-(rnaseq_tracks) [igentile@bamdev2 SlCLV1_SlCLV3]$ cat generate_substitutions.py
 # generate_substitutions.py
 
 def generate_substitutions(sequence):
